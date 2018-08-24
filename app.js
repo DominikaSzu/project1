9b1f73f14436e01bb6308c7e9537cafd45471fe6
Dominika Szukała <dominika.szukala@gmail.com>
2018-08-24 13:43:00 +0200

    Feat: task add

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-   
+   const addBtn = document.querySelector('#addBtn');
+    const inputTask = document.getElementById('inputTask');
+    const listTasks = document.querySelector('.listTasks');
+    
+    addTasks = () => {
+        let insertedInput = inputTask.value;
+        console.log(insertedInput)
+        let task = document.createElement('LI');
+        task.textContent = insertedInput;
+        task.style.cssText= 'list-style-type:circle; text-align:left';
+        inputTask.value = '';
+    }
+
+    addBtn.addEventListener('click', addTasks)
 });
