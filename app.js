ebbe4961beee9b4a85f162d275a10467da016ce0
Dominika Szukała <dominika.szukala@gmail.com>
2018-08-25 21:20:16 +0200

    Modal box added

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
    const addBtn = document.querySelector('#addBtn');
     const inputTask = document.getElementById('inputTask');
     const listTasks = document.querySelector('.listTasks');
+    const modalBox = document.querySelector('.modal');
+    const modalInfo = document.querySelector('.modalText');
+    
     
     addTasks = () => {
         let insertedInput = inputTask.value;
-        console.log(insertedInput)
-        let task = document.createElement('LI');
+        if (insertedInput == '') {
+            modalBox.style.display = 'block';
+        } else {
+          let task = document.createElement('LI');
         task.textContent = insertedInput;
         task.style.cssText= 'list-style-type:circle; text-align:left';
-        inputTask.value = '';
+        listTasks.appendChild(task);
+        inputTask.value = '';  
+        }   
     }
 
-    addBtn.addEventListener('click', addTasks)
+    addBtn.addEventListener('click', addTasks);
+    
+    modalBox.addEventListener('click', function(event) {
+        if (event.target != modalInfo) {
+            modalBox.style.display = 'none';
+        }
+    })
 });
